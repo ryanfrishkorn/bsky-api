da6e2fc0d81189de75998ec5997a7fb1135070d4
Ryan Frishkorn <ryan@bluerift.io>
2026-01-23 17:09:23 -0500

    Use similar table naming scheme

--- scripts/query.sql
+++ scripts/query.sql
@@ -9,21 +9,21 @@
 -- sample database
 attach './data/jetstream.duckdb' as js (read_only);
 
-create temporary table posts_curated as
+create temporary table posts as
 select * from js.posts using sample 1%;
 
 detach js;
 
 create temporary table reputation as
 select did, count(*) as count, count(distinct text) as count_uniq, count_uniq / count as ratio
-    from posts_curated
+    from posts
     group by did;
 
 with top_dids as (
     select did from reputation
     order by ratio limit 100
 ), all_posts as (
-    select * from posts_curated
+    select * from posts
     where did in (select did from top_dids)
 ), total_count as (
     select count(*) as count
@@ -35,6 +35,6 @@ group by did
 order by did_count;
 
 -- information
-select 'posts_curated' as name, count(*) as count from posts_curated
+select 'posts' as name, count(*) as count from posts
 union all
 select 'did_uniq' as did_uniq, count(*) as count from reputation;
